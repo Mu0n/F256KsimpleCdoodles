1e8d541468e93598a29d753167c2abd727e43566
Mu0n <micjuneau@gmail.com>
2024-09-24 14:15:53 -0400

    Added the every frame timer

--- myTimer/.builddir/myTimer.c
+++ myTimer/.builddir/myTimer.c
@@ -5,24 +5,26 @@
 #define TIMER_FRAMES 0
 #define TIMER_SECONDS 1
 
-#define TIMER_QUERY_A 0 // 5 frames
-#define TIMER_QUERY_B 1 // 20 frames
-#define TIMER_QUERY_C 2 // 1s
-#define TIMER_QUERY_D 3 // 3s
-#define TIMER_QUERY_E 4 // 60s
+#define TIMER_QUERY_A 0 // 1 frames
+#define TIMER_QUERY_B 1 // 5 frames
+#define TIMER_QUERY_C 2 // 20 frames
+#define TIMER_QUERY_D 3 // 1s
+#define TIMER_QUERY_E 4 // 3s
+#define TIMER_QUERY_F 5 // 60s
 
-#define TIMER_DELAY_A 5
-#define TIMER_DELAY_B 20
-#define TIMER_DELAY_C 1
-#define TIMER_DELAY_D 3
-#define TIMER_DELAY_E 60
+#define TIMER_DELAY_A 1
+#define TIMER_DELAY_B 5
+#define TIMER_DELAY_C 20
+#define TIMER_DELAY_D 1
+#define TIMER_DELAY_E 3
+#define TIMER_DELAY_F 60
 
 #include "f256lib.h"
 
 
-struct timer_t myTimers[5]; //timer_t structure for setting timers through the kernel
-uint8_t animFrame[5]; // keeps track of which frame it's at in the animation
-uint8_t delays[5]; // injects the TIMER_DELAY_x above into a const struct.
+struct timer_t myTimers[6]; //timer_t structure for setting timers through the kernel
+uint8_t animFrame[6]; // keeps track of which frame it's at in the animation
+uint8_t delays[6]; // injects the TIMER_DELAY_x above into a const struct.
 	
 bool setTimer(const struct timer_t *timer)
 {
@@ -38,6 +40,8 @@ void textSetup(void)
 {
 	textClear();
 	textSetDouble(true,true);
+	textGotoXY(10,3);
+	textPrint("- Every frame");
 	textGotoXY(10,5);
 	textPrint("- Every 5 frames");
 	textGotoXY(10,7);
@@ -59,20 +63,23 @@ void timerSetup(void)
 	animFrame[2]=1;
 	animFrame[3]=1;
 	animFrame[4]=1;
+	animFrame[5]=1;
 	
 	delays[0]=TIMER_DELAY_A;
 	delays[1]=TIMER_DELAY_B;
 	delays[2]=TIMER_DELAY_C;
 	delays[3]=TIMER_DELAY_D;
 	delays[4]=TIMER_DELAY_E;
+	delays[5]=TIMER_DELAY_F;
 	
     myTimers[0].units = TIMER_FRAMES;
 	myTimers[1].units = TIMER_FRAMES;
-	myTimers[2].units = TIMER_SECONDS;
+	myTimers[2].units = TIMER_FRAMES;
 	myTimers[3].units = TIMER_SECONDS;
 	myTimers[4].units = TIMER_SECONDS;
+    myTimers[5].units = TIMER_SECONDS;
 	
-	for(i=0;i<5;i++)
+	for(i=0;i<6;i++)
 	{
 		myTimers[i].absolute = delays[i];
 	}
@@ -82,12 +89,14 @@ void timerSetup(void)
     myTimers[2].cookie = TIMER_QUERY_C;
     myTimers[3].cookie = TIMER_QUERY_D;
     myTimers[4].cookie = TIMER_QUERY_E;
+    myTimers[5].cookie = TIMER_QUERY_F;
 	
 	setTimer(&myTimers[0]);
     setTimer(&myTimers[1]);
 	setTimer(&myTimers[2]);
 	setTimer(&myTimers[3]);
 	setTimer(&myTimers[4]);
+	setTimer(&myTimers[5]);
 }
 
 int main(int argc, char *argv[]) {
@@ -105,7 +114,7 @@ int main(int argc, char *argv[]) {
 			textGotoXY(1,1);
 			textPrint("  ");
 			textPrintInt(curTimer); //top corner sanity check debug
-            textGotoXY(8,5 + 2 * curTimer);
+            textGotoXY(8,3 + 2 * curTimer);
             __putchar(animFrame[curTimer]++); //write the current frame character at the right spot
             if(animFrame[curTimer] > 14) animFrame[curTimer] = 1; //loop back to first anim frame
 			myTimers[curTimer].absolute += delays[curTimer]; //prep the next timer

a61bc837fae5133039e1c257dc12d7af5e5ff41b
Mu0n <micjuneau@gmail.com>
2024-10-25 09:50:35 -0400

    midiStuff debut
    plays random notes on channel 0

---
+++ midiStuff/.builddir/midiStuff.c
@@ -0,0 +1,84 @@
+#include "D:\F256\llvm-mos\code\midiStuff\.builddir\trampoline.h"
+
+#define F256LIB_IMPLEMENTATION
+
+#define MIDI_CTRL 0xDDA0
+#define MIDI_OUT 0xDDA1
+#define MIDI_RX_00_07 0xDDA2
+#define MIDI_RX_08_10 0xDDA3
+
+#define TIMER_FRAMES 0
+#define TIMER_COOKIE 0
+#define TIMER_SECONDS 1
+#define TIMER_DELAY 10
+
+#define SCREENCORNER 0xC000
+
+#include "f256lib.h"
+struct timer_t myTimer; //timer_t structure for setting timer through the kernel
+
+bool setTimer(const struct timer_t *timer)
+{
+    *(uint8_t*)0xf3 = timer->units;
+    *(uint8_t*)0xf4 = timer->absolute;
+    *(uint8_t*)0xf5 = timer->cookie;
+    kernelCall(Clock.SetTimer);
+	return !kernelError;
+}
+
+void setup()
+{
+	
+	textClear();
+	textDefineForegroundColor(0,0xff,0xff,0xff);
+    textGotoXY(0,0); textPrint("running a MIDI test");
+	textGotoXY(0,2); textPrint("0xDDA0");
+	textGotoXY(0,3); textPrint("0xDDA1");
+	textGotoXY(0,4); textPrint("0xDDA2");
+	textGotoXY(0,5); textPrint("0xDDA3");
+	textGotoXY(0,6); textPrint("0xDDA4");
+	textGotoXY(0,7); textPrint("0xDDA5");
+	textGotoXY(0,8); textPrint("0xDDA6");
+	textGotoXY(0,9); textPrint("0xDDA7");
+	
+	myTimer.units = TIMER_FRAMES;
+	myTimer.absolute = TIMER_DELAY;
+    myTimer.cookie = TIMER_COOKIE;
+	setTimer(&myTimer);
+
+}
+
+void refreshPrints()
+{
+	textGotoXY(10,2); textPrintInt(PEEK(0xDDA0));
+	textGotoXY(10,3); textPrintInt(PEEK(0xDDA1));
+	textGotoXY(10,4); textPrintInt(PEEK(0xDDA2));
+	textGotoXY(10,5); textPrintInt(PEEK(0xDDA3));
+	textGotoXY(10,6); textPrintInt(PEEK(0xDDA4));
+	textGotoXY(10,7); textPrintInt(PEEK(0xDDA5));
+	textGotoXY(10,8); textPrintInt(PEEK(0xDDA6));
+	textGotoXY(10,9); textPrintInt(PEEK(0xDDA7));
+}
+int main(int argc, char *argv[]) {
+	uint8_t note;
+	setup();
+	
+    while(true)
+        {
+		kernelNextEvent();
+        if(kernelEventData.type == kernelEvent(timer.EXPIRED))
+            {
+			POKE(MIDI_OUT, 0x80);
+			POKE(MIDI_OUT, note);
+			POKE(MIDI_OUT, 0x4F);
+			myTimer.absolute += TIMER_DELAY;
+            setTimer(&myTimer); // send it to system
+			POKE(1,0);
+			note = 0x20 + randomRead();
+			POKE(MIDI_OUT, 0x90);
+			POKE(MIDI_OUT, note);
+			POKE(MIDI_OUT, 0x4F);
+			refreshPrints();
+            }
+        }
+return 0;}

b995d79d63559d5ab9014c4d9fb5c6caab226f3c
Mu0n <micjuneau@gmail.com>
2024-12-12 06:33:19 -0500

    added sid support, started sid header

---
+++ midiTest/src/musid.h
@@ -0,0 +1,16 @@
+#ifndef MUSID_H
+#define MUSID_H
+
+#define SID1_VOC1    0xD400
+#define SID1_VOC2    0xD407
+#define SID1_VOC3    0xD40E
+
+#define SID2_VOC1    0xD500
+#define SID2_VOC2    0xD507
+#define SID2_VOC3    0xD50E
+
+#define 
+
+#include "f256lib.h"
+
+#endif // MUSID_H

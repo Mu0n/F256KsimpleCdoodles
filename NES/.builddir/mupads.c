73b373e4f641453b39587579885b0bdc78308da3
Mu0n <micjuneau@gmail.com>
2026-01-13 14:14:54 -0500

    polls the SNES, works reliably now

--- NES/.builddir/mupads.c
+++ NES/.builddir/mupads.c
@@ -6,22 +6,28 @@
 void pollNES()
 {
 POKE(PAD_CTRL, CTRL_TRIG | CTRL_MODE_NES);
+	asm("NOP");
+	asm("NOP");
 POKE(PAD_CTRL, CTRL_MODE_NES);
+padPollDelayUntilReady();
 }
 
 void pollSNES()
 {
 POKE(PAD_CTRL, CTRL_TRIG | CTRL_MODE_SNES);
+	asm("NOP");
+	asm("NOP");
 POKE(PAD_CTRL, CTRL_MODE_SNES);
+padPollDelayUntilReady();
 }
 
 bool padPollIsReady()
 {
-return (PEEK(PAD_STAT) & STAT_DONE); //if true, it's ready
+return (PEEK(PAD_CTRL) & STAT_DONE); //if true, it's ready
 }
 
 void padPollDelayUntilReady()
 {
 while((PEEK(PAD_CTRL) & STAT_DONE) != STAT_DONE)
-					;
+		;
 }
--- NES/src/nes.c
+++ NES/src/nes.c
@@ -1,6 +1,7 @@
 #define F256LIB_IMPLEMENTATION
 #include "f256lib.h"
 #include "../src/mupads.h"
+#include "../src/muUtils.h"
 
 int main(int argc, char *argv[]) {
 
@@ -11,13 +12,21 @@ while(true)
 {
 	textGotoXY(0,0);printf("%02x",c++); //show it's not frozen by cycling this on screen
 	pollNES();
-	padPollDelayUntilReady();
-
 	//show what's up
+	textGotoXY(10,4);textPrint("NES pads");
 	textGotoXY(10,5);printf("pad0 status %02x", PEEK(PAD0));
 	textGotoXY(10,6);printf("pad1 status %02x", PEEK(PAD1));
 	textGotoXY(10,7);printf("pad2 status %02x", PEEK(PAD2));
 	textGotoXY(10,8);printf("pad3 status %02x", PEEK(PAD3));
+	
+	
+	pollSNES();
+	textGotoXY(30,4);textPrint("SNES pads");
+	textGotoXY(30,5);printf("pad0 status %02x", PEEK(PAD0));
+	textGotoXY(30,6);printf("pad1 status %02x", PEEK(PAD1));
+	textGotoXY(30,7);printf("pad2 status %02x", PEEK(PAD2));
+	textGotoXY(30,8);printf("pad3 status %02x", PEEK(PAD3));
+	
 }
 
 return 0;}
